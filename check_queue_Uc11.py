188426cfd9855717e6bf08569743a0de7e6028ea
Jampyn <100176998+Jampyn@users.noreply.github.com>
2023-10-06 11:00:41 +0700

    Add files via upload

---
+++ check_queue_Uc11.py
@@ -0,0 +1,59 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import time
+from selenium.webdriver.support.ui import Select
+
+# ระบุพาธของ ChromeDriver 
+chrome_driver_path = "C:/Mos/chromedriver.exe"
+
+# เริ่มต้น WebDriver
+driver = webdriver.Chrome()
+# ขยายหน้าต่างเบราว์เซอร์ให้เต็มหน้าจอ
+driver.maximize_window()
+
+# เปิดเว็บไซต์ของคุณ
+driver.get("https://online-web-mauve.vercel.app/")
+time.sleep(2)
+
+# คลิกปุ่ม "เข้าสู่ระบบ"
+open_modal_button = driver.find_element(By.XPATH, "//span[text()='เข้าสู่ระบบ']")
+open_modal_button.click()
+
+# ระบุ element ของรหัสประจำตัวประชาชนและรหัสผ่านใน Modal
+id_input = driver.find_element(By.XPATH, "//label[text()='รหัสประจำตัวประชาชน']/following-sibling::input")
+password_input = driver.find_element(By.XPATH, "//label[text()='รหัสผ่าน']/following-sibling::input")
+
+# กรอกข้อมูลใน input field
+id_input.send_keys("1729800262972")
+password_input.send_keys("111111")
+
+time.sleep(2)
+
+# คลิกปุ่ม "เข้าสู่ระบบ" ด้วย XPath
+login_button = driver.find_element(By.XPATH, "//button[text()='เข้าสู่ระบบ']")
+login_button.click()
+
+# รอสักครู่เพื่อให้หน้าเว็บโหลดและดำเนินการต่อ
+time.sleep(5)  # รอ 5 วินาที (หรือตามที่คุณต้องการ)
+
+queue_button = driver.find_element(By.ID, 'Home-Queue')
+
+# คลิกที่ปุ่ม
+queue_button.click()
+time.sleep(3)
+
+try:
+    # รอให้ปุ่มเป็นคลิกได้ 10 วินาที (หรือเปลี่ยนเวลาตามที่คุณต้องการ)
+    element = WebDriverWait(driver, 3).until(
+        EC.element_to_be_clickable((By.ID, "Manager_button_status"))
+    )
+
+    # คลิกที่ปุ่ม
+    element.click()
+except Exception as e:
+    print(f"เกิดข้อผิดพลาด: {e}")
+time.sleep(5)
+
